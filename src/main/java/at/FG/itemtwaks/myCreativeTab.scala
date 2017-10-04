6e0d6734e81ca5fd6c02a7fbfdb1e719931ba144
florianGapp <floriangapp12@gmail.com>
2017-10-04 17:08:54 +0200

    Delete myCreativeTab.scala

--- src/main/java/at/FG/itemtwaks/myCreativeTab.scala
+++
@@ -1,9 +0,0 @@
-package at.FG.itemtwaks
-
-import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.item.{Item, ItemStack}
-
-class myCreativeTab(Name:String,I:Item) extends CreativeTabs(Name){
-
-  override def getTabIconItem = new ItemStack((I))
-}
